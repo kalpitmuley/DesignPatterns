cc74d025bdd4871e6048ceaaf6f955fe8a74e1e7
Kalpit <kalpitmuley0@gmail.com>
2020-06-12 07:31:17 +0530

    Corrected code for Rotate by 90 : Link: https://bit.ly/Rotateby90degree

--- Design Patterns/MyProjectPractice/src/ATS/RotateMatrixBy90.java
+++ Design Patterns/MyProjectPractice/src/ATS/RotateMatrixBy90.java
@@ -2,19 +2,14 @@ package ATS;
 // { Driver Code Starts
 //Initial Template for Java
 
-import java.util.*;
-        import java.io.*;
-        import java.lang.*;
+import java.util.Scanner;
 
-class DriverClass
-{
-    public static void main(String[] args)
-    {
+class DriverClass {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
 
-        while (t-- > 0)
-        {
+        while (t-- > 0) {
             int n = sc.nextInt();
             int[][] arr = new int[n][n];
 
@@ -24,12 +19,11 @@ class DriverClass
 
             RotateMatrixBy90 g = new RotateMatrixBy90();
             g.rotate(arr);
-            printMatrix (arr);
+            printMatrix(arr);
         }
     }
 
-    public static void printMatrix(int arr[][])
-    {
+    public static void printMatrix(int arr[][]) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++)
                 System.out.print(arr[i][j] + " ");
@@ -43,31 +37,27 @@ class DriverClass
 
 public class RotateMatrixBy90 {
     static void rotate(int matrix[][]) {
-        DriverClass dc = new DriverClass();
         reverse(matrix);
-        dc.printMatrix(matrix);
         transpose(matrix);
     }
 
     static void reverse(int arr[][]) {
         int n = arr[1].length;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n/2; j++) {
-                int temp = arr[j][i];
-                arr[j][i] = arr[n-(j+1)][i];
-                arr[n-(j+1)][i] = temp;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[i][n - (j + 1)];
+                arr[i][n - (j + 1)] = temp;
             }
         }
     }
+
     static void transpose(int arr[][]) {
-        int n = arr[1].length;
-        System.out.println(n);
-        System.out.println(arr.length);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int temp = arr[i][j];
-                arr[i][j] = arr[j][i];
-                arr[j][i] = temp;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr[0].length; j++) {
+                int temp = arr[j][i];
+                arr[j][i] = arr[i][j];
+                arr[i][j] = temp;
             }
         }
     }
