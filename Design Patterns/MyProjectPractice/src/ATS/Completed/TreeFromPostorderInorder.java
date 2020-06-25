7f0cc76021bc0872f5e8445d019b812928fec6fb
Kalpit <kalpitmuley0@gmail.com>
2020-06-25 10:57:52 +0530

    Added generic TreeNode.java and removed Node class from TreeFromPostorderInorder.java class and reformated the code

--- Design Patterns/MyProjectPractice/src/ATS/Completed/TreeFromPostorderInorder.java
+++ Design Patterns/MyProjectPractice/src/ATS/Completed/TreeFromPostorderInorder.java
@@ -1,22 +1,31 @@
 package ATS.Completed;
 
 // { Driver Code Starts
-import java.util.*;
 
-class Node {
-    int data;
-    Node left;
-    Node right;
-
-    Node(int value) {
-        data = value;
-        left = null;
-        right = null;
-    }
-}
+import java.util.Arrays;
+import java.util.Scanner;
 
 class InorderPostorderToTree {
-    public void preOrder(Node root) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        InorderPostorderToTree ip = new InorderPostorderToTree();
+        int T = sc.nextInt();
+        while (T > 0) {
+            int n = sc.nextInt();
+            int[] inorder = new int[n];
+            int[] postorder = new int[n];
+            for (int i = 0; i < n; i++) inorder[i] = sc.nextInt();
+            for (int i = 0; i < n; i++) postorder[i] = sc.nextInt();
+            GfG g = new GfG();
+            TreeNode root = g.buildTree(inorder, postorder, n);
+            ip.preOrder(root);
+            System.out.println();
+
+            T--;
+        }
+    }
+
+    public void preOrder(TreeNode root) {
         if (root == null) return;
 
         System.out.print(root.data + " ");
@@ -24,37 +33,18 @@ class InorderPostorderToTree {
 
         preOrder(root.right);
     }
-
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        InorderPostorderToTree ip = new InorderPostorderToTree();
-        int T = sc.nextInt();
-        while (T > 0) {
-            int n = sc.nextInt();
-            int inorder[] = new int[n];
-            int postorder[] = new int[n];
-            for (int i = 0; i < n; i++) inorder[i] = sc.nextInt();
-            for (int i = 0; i < n; i++) postorder[i] = sc.nextInt();
-            GfG g = new GfG();
-            Node root = g.buildTree(inorder, postorder, n);
-            ip.preOrder(root);
-            System.out.println();
-
-            T--;
-        }
-    }
 }
 // } Driver Code Ends
 
 
 /* Tree node structure
-class Node
+class TreeNode
 {
     int data;
-    Node left;
-    Node right;
+    TreeNode left;
+    TreeNode right;
 
-        Node(int value)
+        TreeNode(int value)
     {
         data = value;
         left = null;
@@ -68,23 +58,23 @@ class Node
 // size of these arrays
 class GfG {
     // Complete the function
-    Node buildTree(int in[], int post[], int n) {
-        Node root = null;
-        if(n == 1 ) {
-            root = new Node(in[0]);
-            if(in[0] != post[0])
-            root.right = new Node(post[0]);
+    TreeNode buildTree(int[] in, int[] post, int n) {
+        TreeNode root = null;
+        if (n == 1) {
+            root = new TreeNode(in[0]);
+            if (in[0] != post[0])
+                root.right = new TreeNode(post[0]);
         }
-        if(n > 1){
-            root = new Node(post[n-1]);
+        if (n > 1) {
+            root = new TreeNode(post[n - 1]);
             int i = 0;
-            while(in[i] != post[n-1]) i++;
+            while (in[i] != post[n - 1]) i++;
             int lSize = i;
-            int rSize = n - (i+1);
+            int rSize = n - (i + 1);
             root.left = buildTree(Arrays.copyOfRange(in, 0, lSize)
                     , Arrays.copyOfRange(post, 0, lSize), lSize);
-            root.right = buildTree(Arrays.copyOfRange(in, lSize+1, n)
-                    , Arrays.copyOfRange(post, lSize, n-1), rSize);
+            root.right = buildTree(Arrays.copyOfRange(in, lSize + 1, n)
+                    , Arrays.copyOfRange(post, lSize, n - 1), rSize);
         }
         return root;
     }
---
+++ Design Patterns/MyProjectPractice/src/ATS/Completed/TreeNode.java
@@ -0,0 +1,13 @@
+package ATS.Completed;
+
+public class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int value) {
+        data = value;
+        left = null;
+        right = null;
+    }
+}
